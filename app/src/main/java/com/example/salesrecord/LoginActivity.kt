cc65d09348479d01141c59ed25618b611691efa5
Meet Patel <pmeet7898@gmail.com>
2020-01-24 22:38:16 +0530

    Adding Compnay module and changing the icons

--- app/src/main/java/com/example/salesrecord/LoginActivity.kt
+++ app/src/main/java/com/example/salesrecord/LoginActivity.kt
@@ -1,16 +1,49 @@
 package com.example.salesrecord
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+/*
+    private lateinit var loginActivity: LoginActivity
+
+    data class LoginFormState(
+        val usernameError: Int? = null,
+        val passwordError: Int? = null,
+        val isDataValid: Boolean = false
+    )
+
+    private val _loginForm = MutableLiveData<LoginFormState>()
+    private val loginFormState: MutableLiveData<LoginFormState> = _loginForm
+*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+/*
+        loginActivity= ViewModelProviders.of(this, loginFormState)
+            .get(LoginActivity::class.java)
+
+        loginActivity.loginFormState.observe(this@LoginActivity, Observer {
+            val loginState = it ?: return@Observer
+
+            // disable login button unless both username / password is valid
+            btnLogin.isEnabled = loginState.isDataValid
+
+            if (loginState.usernameError != null) {
+                LoginUsername.error = getString(loginState.usernameError)
+            }
+            if (loginState.passwordError != null) {
+                LoginPassword.error = getString(loginState.passwordError)
+            }
+        })
+
+*/
         btnLogin.setOnClickListener {
             if((LoginUsername.text.toString().equals("admin")) and (LoginPassword.text.toString().equals("aadmin"))) {
                 Toast.makeText(this, "Welcome " + LoginUsername.text, Toast.LENGTH_LONG).show()
