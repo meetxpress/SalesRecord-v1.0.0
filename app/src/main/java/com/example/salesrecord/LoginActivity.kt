22670f354ce1a11c53f21156488f61134063688f
Meet Patel <pmeet7898@gmail.com>
2020-02-03 11:43:01 +0530

    validate the login input. //could not be null

--- app/src/main/java/com/example/salesrecord/LoginActivity.kt
+++ app/src/main/java/com/example/salesrecord/LoginActivity.kt
@@ -8,42 +8,10 @@ import androidx.lifecycle.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-/*
-    private lateinit var loginActivity: LoginActivity
-
-    data class LoginFormState(
-        val usernameError: Int? = null,
-        val passwordError: Int? = null,
-        val isDataValid: Boolean = false
-    )
-
-    private val _loginForm = MutableLiveData<LoginFormState>()
-    private val loginFormState: MutableLiveData<LoginFormState> = _loginForm
-*/
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-/*
-        loginActivity= ViewModelProviders.of(this, loginFormState)
-            .get(LoginActivity::class.java)
-
-        loginActivity.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
-
-            // disable login button unless both username / password is valid
-            btnLogin.isEnabled = loginState.isDataValid
-
-            if (loginState.usernameError != null) {
-                LoginUsername.error = getString(loginState.usernameError)
-            }
-            if (loginState.passwordError != null) {
-                LoginPassword.error = getString(loginState.passwordError)
-            }
-        })
-
-*/
         btnLogin.setOnClickListener {
             if((LoginUsername.text.toString().trim() == "") and (LoginPassword.text.toString().trim() == "")){
                 Toast.makeText(this,"Please Enter Username and Password to Login!",Toast.LENGTH_LONG).show()
