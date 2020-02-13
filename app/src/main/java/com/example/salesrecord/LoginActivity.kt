0ff709cd02d3f803dba054a8c0e1c4349bd0cf19
Meet Patel <pmeet7898@gmail.com>
2020-02-13 11:46:39 +0530

    Trust me I made some changes.

--- app/src/main/java/com/example/salesrecord/LoginActivity.kt
+++ app/src/main/java/com/example/salesrecord/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.example.salesrecord
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.*
 import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,31 +23,58 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this,"Please Enter Username and Password to Login!",Toast.LENGTH_LONG).show()
             }
             else {
-                if ((LoginUsername.text.toString().equals("admin")) and (LoginPassword.text.toString().equals("aadmin"))) {
-                    /*
-                    var per=getSharedPreferences("myPref", Context.MODE_PRIVATE)
-                    var edit=per.edit()
-                    edit.putString("user",LoginUsername.text.toString())
-                    edit.commit()*/
-                    var i = Intent(this@LoginActivity, SuperAdminHome::class.java)
-                    startActivity(i)
-                    finish()
-                }
-                else if ((LoginUsername.text.toString().equals("comp")) and (LoginPassword.text.toString().equals("ccomp"))) {
-                    /*var per=getSharedPreferences("myPref", Context.MODE_PRIVATE)
-                    var edit=per.edit()
-                    edit.putString("user",LoginUsername.text.toString())
-                    edit.commit()*/
-                    Toast.makeText(this, "Welcome " + LoginUsername.text, Toast.LENGTH_LONG).show()
-                    var i = Intent(this@LoginActivity, DistributerHome::class.java)
-                    startActivity(i)
-                    finish()
-                }
-                else {
-                    Toast.makeText(this, "Invalid Username and Password", Toast.LENGTH_LONG).show()
-                    LoginPassword.setText("")
-                }
+                var user=LoginUsername.text.toString()
+                var pass=LoginPassword.text.toString()
+                Toast.makeText(this,"Toast Button",Toast.LENGTH_LONG).show()
+                callSevice(user,pass)
             }
+        }
+    }
+
+    fun callSevice(user:String, pass:String){
+        Toast.makeText(this,"Service toast",Toast.LENGTH_LONG).show()
+        try{
+            var client= OkHttpClient()
+
+            var formBody= FormBody.Builder()
+                .add("username",user)
+                .add("password",pass)
+                .build()
+
+            var req= Request.Builder()
+                .url("http://10.0.2.2/SalesRecord/login.php")
+                .post(formBody)
+                .build()
+
+            client.newCall(req).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("Exception",e.toString())
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+                        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                        var str=response.body!!.string()
+                        Log.e("test",str)
+
+                        val jsonObj = JSONObject(str)
+                        var flag=jsonObj.getInt("success")
+                        var message=jsonObj.getString("message")
+
+                        if(flag==1)
+                        {
+                            Toast.makeText(applicationContext,message,Toast.LENGTH_LONG).show()
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(applicationContext,message,Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            })
+        } catch(e: Exception){
+            e.printStackTrace()
         }
     }
 }
--- app/src/main/java/com/example/salesrecord/RegisterCompany.kt
+++ app/src/main/java/com/example/salesrecord/RegisterCompany.kt
@@ -2,6 +2,7 @@ package com.example.salesrecord
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_register_company.*
@@ -27,7 +28,7 @@ class RegisterCompany : AppCompatActivity() {
                 (CompGSTno.toString().length <0) and
                 (CompWebsite.toString().length <0))
             {
-                Toast.makeText(this,CompName.toString().length,Toast.LENGTH_LONG).show()
+               // Toast.makeText(this,CompName.toString().length,Toast.LENGTH_LONG).show()
             }
             else {
                 var coName=CompName.text.toString()
@@ -41,7 +42,7 @@ class RegisterCompany : AppCompatActivity() {
                 var coWeb=CompWebsite.text.toString()
                 callService(coName, coEmail, coCity, coPin, coPhno, coConPer, coLic, coGST, coWeb)
 
-                Toast.makeText(this,CompName.text.toString(),Toast.LENGTH_LONG).show()
+                //Toast.makeText(this,"LOL Else part",Toast.LENGTH_LONG).show()
                 /*CompName.setText("")
                 CompEmail.setText("")  
                 CompCity.setText("")
@@ -57,6 +58,7 @@ class RegisterCompany : AppCompatActivity() {
 
     fun callService(coName:String, coEmail:String, coCity:String, coPin:String, coPhno:String, coConPer:String, coLic:String, coGST:String, coWeb:String){
         try{
+            Toast.makeText(this,"calling service part",Toast.LENGTH_LONG).show()
             var client= OkHttpClient()
 
             var formBody= FormBody.Builder()
@@ -78,14 +80,20 @@ class RegisterCompany : AppCompatActivity() {
 
             client.newCall(req).enqueue(object :Callback {
                 override fun onFailure(call: Call, e: IOException) {
-                    var str=response.body!!.string()
-                    var js=JSONObject()
+                    Log.e("Exception",e.toString())
                 }
 
                 override fun onResponse(call: Call, response: Response) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                }
+                    response.use{
+                        var str=response.body!!.string()
+                        var js=JSONObject(str)
+                       // var =js.getInt("success")
+                        var message=js.getString("message")
+                       // Log.e("id",id.toString())
+                        Log.e("test",message.toString())
 
+                    }
+                }
             })
         }catch(e:Exception){
             e.printStackTrace()
