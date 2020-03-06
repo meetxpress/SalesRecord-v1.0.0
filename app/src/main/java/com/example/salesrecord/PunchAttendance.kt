64144be2b50c11dc44903048d1071e8eb307de1c
Meet Patel <pmeet7898@gmail.com>
2020-03-06 23:59:28 +0530

    Trust me I made some changes.

--- app/src/main/java/com/example/salesrecord/PunchAttendance.kt
+++ app/src/main/java/com/example/salesrecord/PunchAttendance.kt
@@ -1,7 +1,13 @@
 package com.example.salesrecord
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_punch_attendance.*
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
+import kotlin.coroutines.EmptyCoroutineContext.plus
+import kotlin.coroutines.EmptyCoroutineContext.plus
 
 class PunchAttendance : AppCompatActivity() {
 
@@ -12,5 +18,16 @@ class PunchAttendance : AppCompatActivity() {
         //back button on actionbar
         supportActionBar?.setDisplayShowCustomEnabled(true)
 
+        val sdf = SimpleDateFormat("dd/mm/yy")
+        val currentDate = sdf.format(Date())
+
+        val stf = SimpleDateFormat("hh:mm")
+        val curTime = stf.format(Date())
+
+        date1.text=currentDate
+
+        btnAtt1_1.setOnClickListener {
+           textView2.text = "Time: $curTime"
+        }
     }
 }
