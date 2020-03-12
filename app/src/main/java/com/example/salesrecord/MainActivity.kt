f5f95efec65e43fd502de6acfa1aa5f50eec5c7d
Meet Patel <pmeet7898@gmail.com>
2020-03-12 12:03:33 +0530

    Trust me I made some changes.

--- app/src/main/java/com/example/salesrecord/MainActivity.kt
+++ app/src/main/java/com/example/salesrecord/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animation = AnimationUtils.loadAnimation(this@MainActivity, R.anim.logo_splash)
+        logo.startAnimation(animation)
+
+        val animation1 = AnimationUtils.loadAnimation(this@MainActivity, R.anim.logo_text)
+        logo_text.startAnimation(animation1)
 
         Handler().postDelayed({
             var preference=getSharedPreferences("MyPref", Context.MODE_PRIVATE)
