296e48180494edd5457a004d20c789953a14188f
Meet Patel <pmeet7898@gmail.com>
2020-03-20 23:27:14 +0530

    Trust me I made some changes.

---
+++ app/src/main/java/com/example/salesrecord/ViewLeaves.kt
@@ -0,0 +1,13 @@
+package com.example.salesrecord
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewLeaves : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_leaves)
+
+    }
+}
