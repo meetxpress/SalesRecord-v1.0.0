a8e871dd4c2cea3280f19b6a1b7e91e205618870
Meet Patel <55292853+meetxpress@users.noreply.github.com>
2020-03-22 15:56:15 +0530

    Add files via upload

--- phpFiles/getsal.php
+++ phpFiles/getsal.php
@@ -1,10 +1,12 @@
 <?php
     $res = array();
-    if(isset($_POST['emp_id'])){
+    if(isset($_POST['emp_id']) && ($_POST['smonth'])){
 
+		$con=mysqli_connect('localhost', 'root' ,'' ,'salesrecord') or die(mysqli_error());
 		$emp_id=$_POST['emp_id'];
+		$smonth=$_POST['smonth'];
 		
-		$emp_bsal=10000;
+		/*$emp_bsal=10000;
 		$emp_inc=500;
 		$emp_inc1=0;
 		$emp_totsal=15000;
@@ -13,33 +15,54 @@
         $con=mysqli_connect('localhost', 'root' ,'' ,'salesrecord') or die(mysqli_error());
         //$qry=mysqli_query($con,"select esal from employee_master where emp_id='$emp_id'");
         
-        $q1=mysqli_query($con,"select p_target from emp_ptarget where emp_id='$emp_id'");
-		if(mysqli_num_rows($q1) > 0 ){
+        $q1=mysqli_query($con,"select p_target,p_month from emp_ptarget where emp_id='$emp_id'");
+		
+		if(mysqli_num_rows($q1) > 0 )
+		{
 			$r1 = mysqli_fetch_assoc($q1);
 		}
 		
-		if($r1["p_target"] == 0){
-			$q2=mysqli_query($con,"insert into emp_sal (emp_id,emp_bsal,emp_inc,emp_totsal) values ('$emp_id','$emp_bsal','$emp_inc','$emp_totsal')");
-		}else{
-			$q3=mysqli_query($con,"insert into emp_sal (emp_id,emp_bsal,emp_inc,emp_totsal) values ('$emp_id','$emp_bsal','$emp_inc','$emp_totsal')");
-		}
 		
-		$q4=mysqli_query($con,"select emp_bsal,emp_inc,emp_totsal from emp_sal where emp_id='$emp_id'");
+		if(($r1["p_target"] == 0) and ($r1["p_month"] == $smonth))
+		{
+			$q2=mysqli_query($con,"insert into emp_sal (emp_id,emp_bsal,emp_inc,emp_totsal) values ('$emp_id','$emp_bsal','$emp_inc1','$emp_totsal1')");
+			
+			
+			
+		}
+		else if(($r1["p_target"] > 0) and ($r1["p_month"] == $smonth))
+		{
+			$q3=mysqli_query($con,"insert into emp_sal (emp_id,emp_bsal,emp_inc,emp_totsal) values ('$emp_id','$emp_bsal','$emp_inc','$emp_totsal')");
+			
+		}
+		else
+		{
+			//something went wrong;
+			
+		} */
+		
+		$q4=mysqli_query($con,"select emp_bsal,emp_inc,emp_totsal from emp_sal where emp_id='$emp_id' and emp_month='$smonth'");
 		
 		
 		if($q4){
-			$r4 = mysqli_fetch_assoc($q4);	
+			$r4 = mysqli_fetch_assoc($q4);
+			
 			$res["success"] = 1;
 			$res["message"] = "Data found Successfully.";
 			
 			$res["emp_bsal"] = $r4["emp_bsal"];
 			$res["emp_inc"] = $r4["emp_inc"];
 			$res["emp_totsal"] =  $r4["emp_totsal"];
-		
+			
 			echo json_encode($res);
 		}else{
+			
             $res["success"] = 0;
-            $res["message"]="Something went wrong.";
+            $res["message"]="Default Salary";
+			
+			$res["emp_bsal"] = 10000;
+			$res["emp_inc"] = 0;
+			$res["emp_totsal"] = 10000;
     
             echo json_encode($res);
         }
